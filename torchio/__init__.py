f4e7ea840bce0e28baee74df23e1690140804eba
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2016-03-16 11:13:00 +0100

    added a (convenience) method torchio.read(..)

--- torchio/__init__.py
+++ torchio/__init__.py
@@ -7,3 +7,15 @@ from .BinaryFileReader import BinaryFileReader
 
 from .OutputFile       import OutputFile
 from .BinaryFileWriter import BinaryFileWriter
+
+
+#----------------------------------------------------------------------
+def read(fname, mode = "binary"):
+    """reads an object from the given file name"""
+    
+    fin = InputFile(fname, mode)
+
+    return fin.readObject()
+
+#----------------------------------------------------------------------
+    
